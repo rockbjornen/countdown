b490a4788d0b44a66f58f39cded25fdf37d161ef
björn <bjorn.lundkvist@netlight.com>
2018-10-27 10:48:03 +0200

    Updates

---
+++ precache-manifest.ed1e6c83caa1a59ec1d54a0d29e265a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6877d92f4722f96e1a1ecb8440a5d4c1",
+    "url": "/countdown/static/media/shantaram.6877d92f.jpg"
+  },
+  {
+    "revision": "96d06fe5d3a5251dca003e4a226ac89d",
+    "url": "/countdown/static/media/india2.96d06fe5.svg"
+  },
+  {
+    "revision": "2d8efa96241b6e0664482866f75d551f",
+    "url": "/countdown/static/media/butt3.2d8efa96.svg"
+  },
+  {
+    "revision": "bd9e51bd41c1fd515227153538eb8184",
+    "url": "/countdown/static/media/butt2.bd9e51bd.svg"
+  },
+  {
+    "revision": "cde14a88e4bccd2c25d4d9affe1972f1",
+    "url": "/countdown/static/media/butt1.cde14a88.svg"
+  },
+  {
+    "revision": "08800195382208a303b6",
+    "url": "/countdown/static/js/runtime~main.08800195.js"
+  },
+  {
+    "revision": "aa6d148ba8ddeef2e60a",
+    "url": "/countdown/static/js/main.aa6d148b.chunk.js"
+  },
+  {
+    "revision": "545423b136bb6002c220",
+    "url": "/countdown/static/js/1.545423b1.chunk.js"
+  },
+  {
+    "revision": "aa6d148ba8ddeef2e60a",
+    "url": "/countdown/static/css/main.e3572436.chunk.css"
+  },
+  {
+    "revision": "114496f045a45cd70dcf7ce929affef6",
+    "url": "/countdown/index.html"
+  }
+];
